92429cb64d86794c785725ce410227090efdf1d9
navendra1707 <navendraagrawal17@gmail.com>
2023-08-19 11:09:25 +0530

    endpoint change

--- src/endPoints.js
+++ src/endPoints.js
@@ -1,3 +1,3 @@
 export const REGISTER_API = 'register';
 export const LOGIN_API = 'login';
-export const FIND_BY_SYMPTOM = 'find-by-symptoms';
+export const FIND_BY_SYMPTOM = 'doctor/find-by-symptoms';
